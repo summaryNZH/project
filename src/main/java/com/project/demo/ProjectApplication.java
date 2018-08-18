78109d44aed3a9e23cf83a9d76717bef5669d123
ningzhenhang001 <ningzhenhang001@ke.com>
2018-08-19 02:11:01 +0800

    fixed add tk-mybatis bug MapperScan

--- src/main/java/com/project/demo/ProjectApplication.java
+++ src/main/java/com/project/demo/ProjectApplication.java
@@ -1,6 +1,6 @@
 package com.project.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,11 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @Controller
 //@EnableWebMvc
 @SpringBootApplication
-@MapperScan(basePackages = "com.project.demo.mapper")
+@MapperScan(basePackages = {"com.project.demo.mapper"})
 public class ProjectApplication extends WebMvcConfigurerAdapter implements CommandLineRunner {
     private Logger logger = LoggerFactory.getLogger(ProjectApplication.class);
 
